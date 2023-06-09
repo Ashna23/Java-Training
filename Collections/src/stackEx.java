d7a8cac3e63a1f10bfe7e9699e3550c6f459b56c
Ashna23 <69305477+Ashna23@users.noreply.github.com>
2023-06-09 18:46:42 +0530

    Add files via upload

---
+++ Collections/src/stackEx.java
@@ -0,0 +1,17 @@
+import java.util.Stack;
+
+public class stackEx {
+	public static void main(String[] args) {
+		Stack<String> al = new Stack<String>();// 10 -->(cc*3/2)+1  //fifo-->queue,Stack-->FILO
+		al.add("suresh");
+		al.add("mahesh");
+		al.add("rajesh");
+		al.add("somesh");
+		al.add("hitesh");
+		al.add("naresh");
+		al.add("mahesh");
+		System.out.println(al);
+		
+		System.out.println(al.peek());
+	}
+}
